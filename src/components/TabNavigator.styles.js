1b05d0cdec79bc56233fcd6c18c8f861539a3a10
mccullochk <kaileen.mcculloch@gmail.com>
2020-10-06 14:52:37 -0700

    export styled-components inline

--- src/components/TabNavigator.styles.js
+++ src/components/TabNavigator.styles.js
@@ -3,16 +3,14 @@ import { View } from 'react-native'
 import { Text } from 'react-native-paper'
 import theme from '../utils/theme'
 
-const Label = styled(Text)`
+export const Label = styled(Text)`
   color: ${(props) => props.color};
   margin-bottom: 3px;
 `
 
-const CenterView = styled(View)`
+export const CenterView = styled(View)`
   flex: 1;
   justify-content: center;
   align-items: center;
   background: ${theme.colors.background};
 `
-
-export { Label, CenterView }

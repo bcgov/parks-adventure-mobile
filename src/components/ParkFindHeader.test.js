9a54739a7253934085c59f013c8fd64c1f65518a
mccullochk <kaileen.mcculloch@gmail.com>
2020-10-20 08:58:39 -0700

    add tests for the ParkFindHeader component

---
+++ src/components/ParkFindHeader.test.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { render } from '@testing-library/react-native'
+import renderSnapshot from 'react-test-renderer'
+import ParkFindHeader from './ParkFindHeader'
+
+test('Should render using props', () => {
+  const { getByText } = render(
+    <ParkFindHeader count={48} onChangeText={() => {}} onSearch={() => {}} />
+  )
+
+  const title = getByText('48 parks showing')
+
+  expect(title).toBeDefined()
+})
+
+test('Should use the singular "park"', () => {
+  const { getByText } = render(
+    <ParkFindHeader count={1} onChangeText={() => {}} onSearch={() => {}} />
+  )
+
+  const title = getByText('1 park showing')
+
+  expect(title).toBeDefined()
+})
+
+test('Matches snapshot', () => {
+  const tree = renderSnapshot
+    .create(
+      <ParkFindHeader count={4} onChangeText={() => {}} onSearch={() => {}} />
+    )
+    .toJSON()
+  expect(tree).toMatchSnapshot()
+})

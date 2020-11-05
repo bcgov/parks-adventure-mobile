04311cd9bf0d20699182a8c5e19b4f069467626b
Chad Fawcett <me@chadf.ca>
2020-11-05 11:53:09 -0800

    CarouselCard banner and park advisory data

--- src/components/CarouselCard.test.js
+++ src/components/CarouselCard.test.js
@@ -30,3 +30,49 @@ test('renders with no uri and favorited set to true', () => {
   const tree = renderSnapshot.create(<CarouselCard {...park} />).toJSON()
   expect(tree).toMatchSnapshot()
 })
+
+describe('advisory banner', () => {
+  test('if only 1 advisory, display advisory headline', () => {
+    const AdvisoryHeadline = 'Advisory Headline'
+    const park = {
+      title: 'Adventure Park',
+      distance: '297',
+      favorited: true,
+      advisories: [{ Alert: 'N', Headline: AdvisoryHeadline }],
+    }
+
+    const { getByText } = render(<CarouselCard {...park} />)
+    const message = getByText(AdvisoryHeadline)
+
+    expect(message).toBeDefined()
+  })
+
+  test('more than 1 advisory should condense text', () => {
+    const park = {
+      title: 'Adventure Park',
+      distance: '297',
+      favorited: true,
+      advisories: [
+        { Alert: 'N', Headline: 'Advisory 1' },
+        { Alert: 'N', Headline: 'Advisory 2' },
+      ],
+    }
+
+    //  Only Advisories
+    const { getByText, rerender } = render(<CarouselCard {...park} />)
+    let message = getByText('2 Advisories')
+    expect(message).toBeDefined()
+
+    // One Alert and one Advisory
+    park.advisories[0].Alert = 'Y'
+    rerender(<CarouselCard {...park} />)
+    message = getByText('2 Alerts/Advisories')
+    expect(message).toBeDefined()
+
+    //  Only Alerts
+    park.advisories[1].Alert = 'Y'
+    rerender(<CarouselCard {...park} />)
+    message = getByText('2 Alerts')
+    expect(message).toBeDefined()
+  })
+})
